077868e3148a2808103ece1dca9411fd043b9681
Steven Irving <streea@gmail.com>
2018-05-22 15:42:13 -0700

    Added more rooms

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace AdventureGame
     {
         private static string characterName;
         private static string[] items = { "one", "two", "three" };
+        private static bool key = false;
 
         static void Main(string[] args)
         {
@@ -53,7 +54,7 @@ namespace AdventureGame
                     Console.ReadKey();
                     return false;
                 default:
-                    Console.WriteLine("Please say yes or no");
+                    Console.WriteLine("Please say yes or no.");
                     Console.ReadKey();
                     return true;
             }
@@ -63,10 +64,10 @@ namespace AdventureGame
             Console.Clear();
             Console.WriteLine("You are in some room, what are your actions?");
 
-            Console.WriteLine("1) Look at a book");
-            Console.WriteLine("2) Look at the door");
-            Console.WriteLine("3) Jump like an idiot");
-            Console.WriteLine("4) Go to Room 2");
+            Console.WriteLine("1) Look at a book " +
+                "\n2) Look at the door " +
+                "\n3) Jump like an idiot " +
+                "\n4) Go to Room 2");
             Console.Write("\nOption: ");
             string result = Console.ReadLine();
 
@@ -75,15 +76,18 @@ namespace AdventureGame
             switch (result)
             {
                 case "1":
-                    Console.WriteLine("That is a dank ass book");
+                    Console.WriteLine("That is a dank ass book\n" +
+                        "\nGo Back");
                     Console.ReadKey();
                     return true;
                 case "2":
-                    Console.WriteLine("The door is made of wood, and it has a handle");
+                    Console.WriteLine("The door is made of wood, and it has a handle\n" +
+                        "\nGo Back");
                     Console.ReadKey();
                     return true;
                 case "3":
-                    Console.WriteLine("You jump around, like an idiot. As expected");
+                    Console.WriteLine("You jump around, like an idiot. As expected.\n" +
+                        "\nGo Back");
                     Console.ReadKey();
                     return true;
                 case "4":
@@ -96,15 +100,163 @@ namespace AdventureGame
                     return false;
                 default:
                     Console.WriteLine("Please select one of the options");
+                    Console.ReadKey();
                     return true;
             }
         }
         private static bool Room2()
         {
             Console.Clear();
-            Console.WriteLine("You made it to room 2!");
-            Console.WriteLine("\nPress enter to exit");
+            Console.WriteLine("This is the second room" +
+                "\n\n1) Go to Room3" +
+                "\n2) Go to Room4" +
+                "\n3) Go back to Room1");
+            Console.Write("\nOption: ");
+            string result = Console.ReadLine();
+
+            Console.Clear();
+            
+            switch (result)
+            {
+                case "1":
+                    bool room3 = true;
+                    while (room3)
+                    {
+                        room3 = Room3();
+                    }
+                    Console.ReadKey();
+                    return false;
+                case "2":
+                    bool room4 = true;
+                    while (room4)
+                    {
+                        room4 = Room4();
+                    }
+                    Console.ReadKey();
+                    return false;
+                case "3":
+                    bool room1 = true;
+                    while (room1)
+                    {
+                        room1 = Room1();
+                    }
+                    Console.ReadKey();
+                    return false;
+            }
             return false;
+        }
+        private static bool Room3()
+        {
+            // in this room find a key!
+            Console.Clear();
+            Console.WriteLine("Wow, another room.\n" +
+                "\n1) Look at the table" +
+                "\n2) Get out of this boring room");
+
+            Console.Write("\nOption: ");
+            string result = Console.ReadLine();
+
+            Console.Clear();
+
+            switch (result)
+            {
+                case "1":
+                    Key();
+                    break;
+                case "2":
+                    bool room2 = true;
+                    while (room2)
+                    {
+                        room2 = Room2();
+                    }
+                    Console.ReadKey();
+                    return false;
+                default:
+                    Console.WriteLine("Please select one of the options");
+                    Console.ReadKey();
+                    return true;
+            }
+
+            return false;
+        }
+        private static bool Room4()
+        {
+            //locked door room!
+            Console.Clear();
+            Console.WriteLine("This sure is a room! There's some stuff in here.\n" +
+                "\n1) Look around" +
+                "\n2) Look at roof" +
+                "\n3) Go back to Room2" +
+                "\n4) Go to Room5");
+            return false;
+        }
+        private static bool Room5()
+        {
+            // somehow get a puzzle?
+            return false;
+        }
+        private static bool Room6()
+        {
+            //end of the game
+            return false;
+        }
+        private static void Key()
+        {
+            Console.Clear();
+            
+            if (key == false)
+            { 
+            Console.Write("There's a ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("key");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(" on the table.");
+            Console.Write("\n\nTake the key?" +
+                "\nOption: ");
+            string choice = Console.ReadLine();
+            choice = choice.ToLower();
+                Console.Clear();
+
+                switch (choice)
+                {
+                    case "yes":
+                        Console.Write("You picked up the ");
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write("key");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write("!");
+                        key = true;
+                        Console.ReadKey();
+                        Room3();
+                        break;
+                    case "no":
+                        Console.Write("You leave the ");
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write("key");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write(" on the table.");
+                        key = false;
+                        Console.ReadKey();
+                        Room3();
+                        break;
+                    default:
+                        Console.WriteLine("Please select one of the options.");
+                        Console.ReadKey();
+                        Key();
+                        break;
+                }
+            }
+            else
+            {
+                Console.Write("The table is empty, you already took the ");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("key");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write(".");
+                Console.ReadKey();
+                key = true;
+                Room3();
+            }
         }
     }
 }
